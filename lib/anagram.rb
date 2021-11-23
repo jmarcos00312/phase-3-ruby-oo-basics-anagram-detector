be26cf8cf389ac471c3bd86efd82018118eb9b8a
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-23 11:06:43 -0600

    commit tree c1bde449839b876b5831012c6fa092437b86e4ad

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
+require 'pry'
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+     words.filter do |w|
+      w.split('').sort == word.split('').sort
+    end
+  end
+end
